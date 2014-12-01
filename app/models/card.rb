97ae758b5ca8d6c4def9c23b4b837d368960eb43
kasei_san <kasei.san@gmail.com>
2014-12-02 00:41:40 +0900

    Parse 反転カード on Card#parse

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,25 +1,35 @@
 class Card < ActiveRecord::Base
   def self.parse(text)
-    hash = text.sub(/^　タイプ：.*$\n/, '\&テキスト：')     # テキストだけヘッダが無いので追加
-            .gsub("　", '').gsub(/([^\r])\n/, "\\1\n\n") # テキストだけ\r\nで改行されている
-            .split("\n\n").map{|str| str.split('：')}.to_h
 
-    hash['日本語名'].sub!(/（.*$/, '')
-    hash['テキスト'].sub!(/\r/, '')
-    hash['コスト'].tr!('０-９', '0-9')
+    parse_text = text.dup
+    parse_text.gsub!(/^　タイプ：.*$\n/, '\&テキスト：')      # テキストだけヘッダが無いので追加
+    parse_text.gsub!("　", '').gsub!(/([^\r])\n/, "\\1\n\n") # テキストだけ\r\nで改行されている
+    parse_text.gsub!(/^(イラスト|セット|稀少度).*$\n\n/, '') # 変身・反転カードのときめんどいので削除
 
-    converted_mana_cost = hash['コスト'].scan(/\([^\)]+\)/).map{|v| v.sub!(/\(([^\)]+)\)/, '\1')}.inject(0) do |result, cost|
-      result + (cost =~ /\A\d+\z/ ? cost.to_i : 1)
+    current_flg = true
+    parse_text.split("\n\n").map{|str| str.split('：', 2)}.each_slice(6).map(&:to_h).map do |hash|
+      hash['日本語名'].sub!(/（.*$/, '')
+      hash['テキスト'].sub!(/\r/, '')
+      hash['コスト'].tr!('０-９', '0-9')
+
+      if current_flg
+        converted_mana_cost = hash['コスト'].scan(/\([^\)]+\)/).map{|v| v.sub!(/\(([^\)]+)\)/, '\1')}.inject(0) do |result, cost|
+          result + (cost =~ /\A\d+\z/ ? cost.to_i : 1)
+        end
+        current_flg = false
+      else
+        converted_mana_cost = -1
+      end
+
+      Card.new({
+        name:                 hash['英語名'],
+        japanese_name:        hash['日本語名'],
+        mana_cost:            hash['コスト'],
+        card_type:            hash['タイプ'],
+        text:                 hash['テキスト'],
+        power_toughness:      hash['Ｐ／Ｔ'],
+        converted_mana_cost: converted_mana_cost
+      })
     end
-
-    Card.new({
-      name:                 hash['英語名'],
-      japanese_name:        hash['日本語名'],
-      mana_cost:            hash['コスト'],
-      card_type:            hash['タイプ'],
-      text:                 hash['テキスト'],
-      power_toughness:      hash['Ｐ／Ｔ'],
-      converted_mana_cost: converted_mana_cost
-    })
   end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Card, :type => :model do
   describe '#parse' do
+
     subject { Card.parse(text) }
+
     let(:text){ <<-EOS }
 　英語名：Abattoir Ghoul
 日本語名：肉切り屋のグール（にくきりやのぐーる）
@@ -15,16 +17,18 @@ RSpec.describe Card, :type => :model do
 　セット：Innistrad
 　稀少度：アンコモン
     EOS
-    it { expect(subject.name).to eq('Abattoir Ghoul') }
-    it { expect(subject.japanese_name).to eq('肉切り屋のグール') }
-    it { expect(subject.mana_cost).to eq('(3)(黒)') }
-    it { expect(subject.card_type).to eq('クリーチャー --- ゾンビ(Zombie)') }
-    it { expect(subject.text).to eq(<<-EOS.strip) }
+
+    it { expect(subject.size).to eq(1) }
+    it { expect(subject.first.name).to eq('Abattoir Ghoul') }
+    it { expect(subject.first.japanese_name).to eq('肉切り屋のグール') }
+    it { expect(subject.first.mana_cost).to eq('(3)(黒)') }
+    it { expect(subject.first.card_type).to eq('クリーチャー --- ゾンビ(Zombie)') }
+    it { expect(subject.first.text).to eq(<<-EOS.strip) }
 先制攻撃
 このターン、肉切り屋のグールによってダメージを与えられたクリーチャーが１体死亡するたび、あなたはそのクリーチャーのタフネスに等しい点数のライフを得る。
     EOS
-    it { expect(subject.power_toughness).to eq('3/2') }
-    it { expect(subject.converted_mana_cost).to eq(4) }
+    it { expect(subject.first.power_toughness).to eq('3/2') }
+    it { expect(subject.first.converted_mana_cost).to eq(4) }
 
     context '混成マナコスト' do
       let(:text){ <<-EOS }
@@ -32,8 +36,8 @@ RSpec.describe Card, :type => :model do
 日本語名：アゾリウスのギルド魔道士（あぞりうすのぎるどまどうし）
 　コスト：(白/青)(白/青)
 　タイプ：クリーチャー --- ヴィダルケン(Vedalken)・ウィザード(Wizard)
-（(白/青)は(白)でも(青)でも支払うことができる。）
-(２)(白)：クリーチャー１体を対象とし、それをタップする。
+（(白/青)は(白)でも(青)でも支払うことができる。）#{0x0D.chr}
+(２)(白)：クリーチャー１体を対象とし、それをタップする。#{0x0D.chr}
 (２)(青)：起動型能力１つを対象とし、それを打ち消す。（マナ能力は対象にできない。）
 　Ｐ／Ｔ：2/2
 イラスト：Christopher Moeller
@@ -41,13 +45,54 @@ RSpec.describe Card, :type => :model do
 　稀少度：アンコモン
       EOS
 
-      it { expect(subject.mana_cost).to eq('(白/青)(白/青)') }
-      it { expect(subject.converted_mana_cost).to eq(2) }
-
-
+      it { expect(subject.first.mana_cost).to eq('(白/青)(白/青)') }
+      it { expect(subject.first.converted_mana_cost).to eq(2) }
     end
-    it '混成(ローウィンの)マナコスト'
+
+    context '反転カード' do
+      let(:text){ <<-EOS }
+　英語名：Student of Elements
+日本語名：精霊の学び手（せいれいのまなびて）
+　コスト：(１)(青)
+　タイプ：クリーチャー --- 人間(Human)・ウィザード(Wizard)
+精霊の学び手が飛行を持っているとき、精霊の学び手を反転する。
+　Ｐ／Ｔ：1/1
+イラスト：Ittoku
+　英語名：Tobita, Master of Winds
+日本語名：風の達人、鳶太（かぜのたつじんとびた）
+　コスト：(１)(青)
+　タイプ：伝説のクリーチャー --- 人間(Human)・ウィザード(Wizard)
+あなたがコントロールするクリーチャーは飛行を持つ。
+　Ｐ／Ｔ：3/3
+イラスト：Ittoku
+　セット：Champions of Kamigawa
+　稀少度：アンコモン
+      EOS
+
+      it { expect(subject.size).to eq(2) }
+
+      it { expect(subject.first.name).to eq('Student of Elements') }
+      it { expect(subject.first.japanese_name).to eq('精霊の学び手') }
+      it { expect(subject.first.mana_cost).to eq('(1)(青)') }
+      it { expect(subject.first.card_type).to eq('クリーチャー --- 人間(Human)・ウィザード(Wizard)') }
+      it { expect(subject.first.text).to eq(<<-EOS.strip) }
+精霊の学び手が飛行を持っているとき、精霊の学び手を反転する。
+      EOS
+      it { expect(subject.first.power_toughness).to eq('1/1') }
+      it { expect(subject.first.converted_mana_cost).to eq(2) }
+
+      it { expect(subject.last.name).to eq('Tobita, Master of Winds') }
+      it { expect(subject.last.japanese_name).to eq('風の達人、鳶太') }
+      it { expect(subject.last.mana_cost).to eq('(1)(青)') }
+      it { expect(subject.last.card_type).to eq('伝説のクリーチャー --- 人間(Human)・ウィザード(Wizard)') }
+      it { expect(subject.last.text).to eq(<<-EOS.strip) }
+あなたがコントロールするクリーチャーは飛行を持つ。
+      EOS
+      it { expect(subject.last.power_toughness).to eq('3/3') }
+      it { expect(subject.last.converted_mana_cost).to eq(-1) }
+    end
+
+
     it '変身カード'
-    it '反転カード'
   end
 end
